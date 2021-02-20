fe6bcc60bdf73571bc0cef6f90ad609a4432a3c2
Cavid Ahadzada <cavidahadov7@gmail.com>
2021-02-20 16:44:08 +0400

    initial commit

---
+++ Shop1.js
@@ -0,0 +1,17 @@
+function Increase (param) {
+    var button = document.getElementById(param).value
+    if(button==15){
+       button=14
+       alert('You can buy maximum 15 form 1 product')
+    }
+    document.getElementById(param).value= ++button
+    
+ }
+ function Decrease (param){
+       var button = document.getElementById(param).value
+       if(button==1){
+          button=2
+       }
+       document.getElementById(param).value = --button
+ }
+ 
